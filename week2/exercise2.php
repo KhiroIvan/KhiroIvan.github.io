6f31a1891c12b31b22436a8b8316e086a111bf64
Ivan <ivantan211012@dasein.edu.my>
2022-06-15 11:40:53 +0800

    New work

---
+++ week2/exercise2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+
+    <?php
+    $x = 100;
+    while ($x >= 1) {
+        echo "number $x";
+        echo "<br>";
+        $x = $x - 1;
+    }
+    ?>
+
+</body>
+
+</html>
