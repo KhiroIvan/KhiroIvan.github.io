08bc36ce8f78a873f9699ad8db03bd99c97d1f50
Ivan <ivantan211012@dasein.edu.my>
2022-06-15 11:16:53 +0800

    Test run

---
+++ week2/exercise1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+    <?php
+    $surname = "Tan";
+    $given = "Ivan";
+    $age = "18";
+    ?>
+
+    <?php
+    echo "my name is $surname $given.";
+    echo "<br>";
+    echo "I am <b>$age</b> years old.";
+    ?>
+</body>
+
+</html>
