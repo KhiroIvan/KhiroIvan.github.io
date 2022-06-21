32de66ac0acf4e61a917f7c29ee7814421110aea
Ivan <ivantan211012@dasein.edu.my>
2022-06-21 15:41:07 +0800

    Added new work

---
+++ week2/homework1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+
+    <?php
+    echo date('d F Y');
+    ?>
+
+    <?php
+    echo date('d F Y');
+    ?>
+
+    <?php
+    echo date('d F Y');
+    ?>
+
+</body>
+
+</html>
