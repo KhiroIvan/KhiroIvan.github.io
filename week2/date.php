1e9f042ff5925f2b7d176140948940c5737be154
Ivan <ivantan211012@dasein.edu.my>
2022-06-21 16:04:58 +0800

    Added change into date

--- week2/date.php
+++ week2/date.php
@@ -2,22 +2,69 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="css/homework1.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <title>date</title>
 </head>
 
 <body>
 
-    <?php
-    echo date('d F Y');
-    ?>
+<div id="container">
+    <div id="starting">
+        <div class="first">
+            <?php
+            echo date("d");
+            ?>
+        </div>
 
-    <?php
-    echo date('d F Y');
-    ?>
+        <br>
 
-    <?php
-    echo date('d F Y');
-    ?>
+        <div class="second">
+            <?php
+            echo date("M Y");
+            ?>
+        </div>
+    </div>
+    <!--the end of starting-->
+    <br />
+
+    <div id="middle">
+        <div class="third">
+            <?php
+            echo date("d");
+            ?>
+        </div>
+
+        <div class="fourth">
+            <?php
+            echo date("M Y");
+            ?>
+        </div>
+    </div>
+    <!--the end of middle-->
+    
+
+    <div id="last">
+        <div class="fifth">
+            <?php
+            echo date("d");
+            ?>
+        </div>
+
+        <div class="sixth">
+            <?php
+            echo date("M");
+
+            echo "<br>";
+
+            echo date("Y");
+            ?>
+
+        </div>
+    </div>
+    <!--the end of last-->
+
+    </div><!--the end of container-->
 
 </body>
 
