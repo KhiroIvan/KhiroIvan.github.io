3e49d15d85759a909430bce62b98db52e65c4195
Ivan <ivantan211012@dasein.edu.my>
2022-06-22 11:02:51 +0800

    Done

--- week3/mark.php
+++ week3/mark.php
@@ -3,17 +3,25 @@
 <body>
 
 <?php
-$mark = 74;
+$mark = 100;
 var_dump($mark);
 
-if ($mark >= 80) {
+if ($mark >= 80 && $mark <= 100) {
 	echo "Distinction";
-} elseif ($mark >= 60) {
+    	if ($mark == 100) {
+        	echo "Well Done";
+        } 
+} elseif ($mark >= 60 && $mark <= 79) {
 	echo "Good";
-} elseif ($mark >= 40) {
+    
+} elseif ($mark >= 40 && $mark <= 59) {
 	echo "Pass";
-} else { 
+    
+} elseif ($mark >= 0 && $mark <= 39) { 
 	echo "Fail";
+    
+} else {
+    echo "Error";
 }
 
 ?>
