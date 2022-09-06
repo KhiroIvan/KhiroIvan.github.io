dede5f35390de57516af291da26394e25affea0c
Ivan <ivantan211012@dasein.edu.my>
2022-09-06 22:19:02 +0800

    Sorted

--- Project/customer_delete.php
+++ Project/customer_delete.php
@@ -1,16 +1,29 @@
 <?php
 // include database connection
 include 'config/database.php';
+
+
 try {     
     // get record ID
     // isset() is a PHP function used to verify if a value is there or not
     $id = isset($_GET['id']) ? $_GET['id'] :  die('ERROR: Record ID not found.');
 
     // delete query
+    $query = "SELECT image FROM customer WHERE id = ?";
+    $stmt = $con->prepare($query);
+    $stmt->bindParam(1, $id);
+    $stmt->execute();
+    
+    // store retrieved row to a variable
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $image = $row['image'];
+   
+    // delete query
     $query = "DELETE FROM customer WHERE id = ?";
     $stmt = $con->prepare($query);
     $stmt->bindParam(1, $id);
-    
+    unlink("uploads/".$image); 
+
     if($stmt->execute()){
         // redirect to read records page and
         // tell the user record was deleted
